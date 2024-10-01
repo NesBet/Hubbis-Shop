74522f7938f129040ee96bc3ef3997271f8ea3c8
NesBet <nehmz254@gmail.com>
2024-10-01 12:14:51 +0300

    Debugging PlaceOrderPage.js

--- frontend/src/pages/PlaceOrderPage.js
+++ frontend/src/pages/PlaceOrderPage.js
@@ -1,3 +1,4 @@
+// Import necessary components
 import { useEffect } from 'react'
 import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
