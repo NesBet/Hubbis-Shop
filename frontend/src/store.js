7c42703e54f1b53d5ffaf4aaf2695c48492109ba
NesBet <nehmz254@gmail.com>
2024-10-01 12:19:13 +0300

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,3 +1,4 @@
+// Import modules
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
